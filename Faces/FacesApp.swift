7620f9e44760e07ce6387a3ebef2698abaeb48d7
Kvend-ac <kvenii@ya.ru>
2024-08-22 12:39:01 +0300

    Initial Commit

---
+++ Faces/FacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacesApp.swift
+//  Faces
+//
+//  Created by Сергей Прасолов on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
