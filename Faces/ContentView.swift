ab38161b4faee543e170ce0277abcfcdd836b064
Kvend-ac <kvenii@ya.ru>
2024-08-22 16:23:53 +0300

    Написал простой функционал добавления фотографий в список просмотра.
    На данном этапе данные не сохраняются

--- Faces/ContentView.swift
+++ Faces/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var image: Image?
+    
+    @State private var faces = [Face]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    ForEach(faces) { face in
+                        face.photo
+                            .resizable()
+                            .scaledToFit()
+                        Text(face.name)
+                    }
+                }
+                .onChange(of: selectedItem) {
+                    loadImage()
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        PhotosPicker("Select photo", selection: $selectedItem)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func loadImage() {
+        
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            faces.append(Face(id: UUID(), photoData: imageData, name: "New photo", description: "desc"))
+        }
+        
     }
 }
 
---
+++ Faces/Face.swift
@@ -0,0 +1,29 @@
+//
+//  Face.swift
+//  Faces
+//
+//  Created by Сергей Прасолов on 22.08.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Face: Codable, Equatable, Identifiable {
+    let id: UUID
+    var photoData: Data?
+    var name: String
+    var description: String
+    
+    var photo: Image {
+        if let inputImage = UIImage(data: photoData!) {
+            return Image(uiImage: inputImage)
+        } else {
+            return Image(systemName: "photo.artframe")
+        }
+    }
+    
+    #if DEBUG
+    static let example = Face(id: UUID(), name: "Alice", description: "Person from a book")
+    #endif
+    
+}
